8a20048f8160ab3984106e4547504a99a93c9245
Zeracronius <zeracronius@outlook.dk>
2024-09-23 19:20:04 +0200

    Initial commit

---
+++ Source/Mod/Initalizer.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace LockableDoors.Mod
+{
+	[StaticConstructorOnStartup]
+	internal static class Initalizer
+	{
+        static Initalizer()
+        {
+
+			Harmony Harmony = new Harmony("DynamicTradeInterfaceMod");
+		}
+    }
+}
---
+++ Source/Mod/LockableDoorsMod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace LockableDoors.Mod
+{
+	internal class LockableDoorsMod : Verse.Mod
+	{
+		public LockableDoorsMod(ModContentPack content) : base(content)
+		{
+		}
+	}
+}
